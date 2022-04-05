6bbd02e3c49c2d0128867e707c21d8c7d043412d
Naman <namanbiswajit.code@gmail.com>
2022-04-05 14:54:54 +0530

    Added base files

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(port=5000, debug=True)
