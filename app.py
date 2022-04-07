7c27ce298e01ab3acb92072af0af06020aae1741
Naman <namanbiswajit.code@gmail.com>
2022-04-07 15:52:30 +0530

    Added 404 Error page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
 
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route('/')
+def login():
+    return redirect('/explore')
+
+@app.route('/explore')
 def index():
     return render_template('index.html', explore="active")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 app.run(port=5000, debug=True)
--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,13 +1,9 @@
 window.onload = function () {
-    const bars = document.getElementById('bars');
-    const ul = document.getElementById('navbar-ul');
+	const bars = document.getElementById("bars");
+  const ul = document.getElementById("navbar-ul");
 
-    console.log(bars);
-    console.log(ul);
-    
-    bars.addEventListener('click', () => {
-        ul.classList.toggle('show');
-        console.log("Event Handler: Clicked on hamburger");
-    });
+  bars.addEventListener("click", () => {
+    ul.classList.toggle("show");
+    console.log("Event Handler: Clicked on hamburger");
+  });
 };
-
