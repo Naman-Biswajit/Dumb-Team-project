2ea57202d6853b7c179bf80159a1a9dd2f855079
Naman <namanbiswajit.code@gmail.com>
2022-04-17 09:33:42 +0530

    Added basic structure for task list table

--- app.py
+++ app.py
@@ -8,16 +8,20 @@ app = Flask(__name__, template_folder='templates')
 def login():
     return redirect('/explore')
 
+
 @app.route('/explore')
 def index():
     return render_template('explore.html', explore="active")
+
 
 @app.route('/planner')
 def planner():
     return render_template('planner.html', explore="active")
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
+
 
 app.run(port=5000, debug=True)
