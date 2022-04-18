75a4355f77306309e5b75cef57b3d8891f966d75
Naman <namanbiswajit.code@gmail.com>
2022-04-18 10:13:34 +0530

    Added login page

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+from email.errors import MessageError
 import os
 import dotenv
 
 from pymongo import MongoClient
 from flask import Flask, redirect
 from flask import render_template, request
-from utils.login import insert_user
+from utils.user import insert_user, login_user
 
 dotenv.load_dotenv()
 MONGO_URL = os.getenv('MONGO_URL')
@@ -22,18 +23,31 @@ def index():
     return redirect('/register')
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
-        
-        out = insert_user(username, password)
+    if request.method == 'POST':
+        logged_in = login_user(request.form['username'], request.form['password'])
 
-        if out is True:
+        if type(logged_in) == str:
+            print(logged_in)
+            return render_template('login.html', error=True, message=logged_in)
+
+        else:
+            print(logged_in)
             return redirect('/explore')
-        
-        else: 
+
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        inserted = insert_user(request.form['username'], request.form['password'])
+
+        if inserted is True:
+            return redirect('/explore')
+
+        else:
             return render_template('register.html', error=True)
 
     return render_template('register.html')
